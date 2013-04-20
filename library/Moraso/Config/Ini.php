a0b02d505de229670c2be66b891cf8dd2186a29e
Christian Kehres <c.kehres@webtischlerei.de>
2013-04-20 12:03:09 +0200

    testing

---
+++ library/Moraso/Config/Ini.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @author Christian Kehres <c.kehres@webtischlerei.de>
+ * @copyright (c) 2013, webtischlerei <http://www.webtischlerei.de>
+ */
+class Moraso_Config_Ini {
+
+    protected function __construct() {
+        
+    }
+
+    public static function getInstance($ini) {
+
+        $env = Moraso_Util::getEnv();
+
+        $config = new Zend_Config_Ini('application/configs/config.ini', $env, array(
+            'allowModifications' => true
+        ));
+
+        if ($ini != 'backend') {
+            $client_config = new Zend_Config_Ini('application/configs/' . $ini . '.ini', $env, array(
+                'allowModifications' => true
+            ));
+
+            $config->merge($client_config);
+        }
+
+        return $config;
+    }
+
+}
