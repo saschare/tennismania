d029e93763d8733662951b0c6c23030e69cee27a
Christian Kehres <c.kehres@webtischlerei.de>
2013-05-08 13:01:42 +0200

    eav system optimiert

--- library/Moraso/Db.php
+++ library/Moraso/Db.php
@@ -2,7 +2,7 @@
 
 /**
  * @author Christian Kehres <c.kehres@webtischlerei.de>
- * @copyright (c) 2012, webtischlerei <http://www.webtischlerei.de>
+ * @copyright (c) 2013, webtischlerei <http://www.webtischlerei.de>
  */
 class Moraso_Db extends Aitsu_Db {
 
@@ -32,7 +32,7 @@ class Moraso_Db extends Aitsu_Db {
         $orderBy = count($orders) == 0 ? '' : 'order by ' . implode(', ', $orders);
         $groupBy = count($groups) == 0 ? '' : 'group by ' . implode(', ', $groups);
 
-        $results = Aitsu_Db::fetchAll('' .
+        $results = Moraso_Db::fetchAll('' .
                         $baseQuery .
                         ' ' . $where .
                         ' ' . $groupBy .
@@ -48,6 +48,80 @@ class Moraso_Db extends Aitsu_Db {
         }
 
         return $return;
+    }
+
+    /**
+     * @since 1.14.0-1
+     */
+    public static function delete($from, array $where) {
+
+        $whereClause = array();
+        $whereValues = array();
+
+        foreach ($where as $key => $value) {
+            $clause = '=';
+
+            if (is_array($value)) {
+                $clause = $value['clause'];
+                $value = $value['value'];
+            }
+
+            $whereClause[] = $key . ' ' . $clause . ':value_' . $key;
+            $whereValues[':value_' . $key] = $value;
+        }
+
+        Moraso_Db::query('delete from ' . $from . ' where ' . implode(' and ', $whereClause) . '', $whereValues);
+    }
+
+    /**
+     * @since 1.14.0-1
+     */
+    public static function simpleFetch($select, $from, array $where, $limit = 1, $caching = 0) {
+
+        $whereClause = array();
+        $whereValues = array();
+
+        foreach ($where as $key => $value) {
+            $clause = '=';
+
+            if (is_array($value)) {
+                $clause = $value['clause'];
+                $value = $value['value'];
+            }
+
+            $whereClause[] = $key . ' ' . $clause . ':value_' . $key;
+            $whereValues[':value_' . $key] = $value;
+        }
+
+        if ($caching === 0) {
+            if ($limit === 1) {
+                if (is_array($select)) {
+                    return Moraso_Db::fetchRow('select ' . implode(', ', $select) . ' from ' . $from . ' where ' . implode(' and ', $whereClause) . '', $whereValues);
+                }
+
+                return Moraso_Db::fetchOne('select ' . $select . ' from ' . $from . ' where ' . implode(' and ', $whereClause) . '', $whereValues);
+            } else {
+                if (!is_array($select)) {
+                    $select = str_split($select);
+                }
+
+                return Moraso_Db::fetchAll('select ' . implode(', ', $select) . ' from ' . $from . ' where ' . implode(' and ', $whereClause) . '', $whereValues);
+            }
+        } else {
+            if ($limit === 1) {
+                if (is_array($select)) {
+                    return Moraso_Db::fetchRowC($caching, 'select ' . implode(', ', $select) . ' from ' . $from . ' where ' . implode(' and ', $whereClause) . '', $whereValues);
+                }
+
+                return Moraso_Db::fetchOneC($caching, 'select ' . $select . ' from ' . $from . ' where ' . implode(' and ', $whereClause) . '', $whereValues);
+            } else {
+                if (!is_array($select)) {
+                    $select = str_split($select);
+                }
+
+                return Moraso_Db::fetchAllC($caching, 'select ' . implode(', ', $select) . ' from ' . $from . ' where ' . implode(' and ', $whereClause) . '', $whereValues);
+            }
+        }
     }
 
 }
--- library/Moraso/Eav.php
+++ library/Moraso/Eav.php
@@ -6,76 +6,38 @@
  */
 class Moraso_Eav {
 
-    public static function createEntity() {
-
-        return Moraso_Db::query('insert into _eav_entity (entity_id) values (NULL)')->getLastInsertId();
-    }
-
-    public static function checkIfAttributeAlreadyExist($attribute_set_id, $attribute_alias) {
-
-        $attribute_id = self::getAttributeIdByAlias($attribute_set_id, $attribute_alias);
-
-        if (!$attribute_id) {
-            $attribute_id = self::createAttribute($attribute_set_id, $attribute_alias);
-        }
-
-        return $attribute_id;
-    }
-
-    public static function checkIfAttributeSetAlreadyExist($attribute_set_name) {
-
-        $attribute_set_id = self::getAttributeSetId($attribute_set_name);
-
-        if (!$attribute_set_id) {
-            $attribute_set_id = self::createAttributeSet($attribute_set_name);
-        }
-
-        return $attribute_set_id;
-    }
-
-    public static function createAttribute($set_id, $alias) {
-
-        return Moraso_Db::query('' .
-                        'insert into ' .
-                        '   _eav_attribute ' .
-                        '   (attribute_alias, attribute_set_id) ' .
-                        'values ' .
-                        '   ("' . $alias . '", "' . $set_id . '")'
-                )->getLastInsertId();
-    }
-
-    public static function createAttributeSet($set_name) {
-
-        return Moraso_Db::query('' .
-                        'insert into ' .
-                        '   _eav_attribute_set ' .
-                        '   (attribute_set_name) ' .
-                        'values ' .
-                        '   ("' . $set_name . '")'
-                )->getLastInsertId();
-    }
-
     public static function deleteEntity($id) {
 
-        Moraso_Db::query('' .
-                'delete from ' .
-                '   _eav_entity ' .
-                'where ' .
-                '   entity_id =:id', array(
-            ':id' => $id
-        ));
+        Moraso_Db::delete('_eav_entity', array('entity_id' => $id));
     }
 
     public static function setValues($attribute_set, $data) {
 
-        $id = $data['id'];
+        $id = isset($data['id']) && !empty($data['id']) ? $data['id'] : Moraso_Db::query('insert into _eav_entity (entity_id) values (NULL)')->getLastInsertId();
 
         unset($data['id']);
+
         foreach ($data as $attribute_alias => $value) {
 
-            $attribute_set_id = self::checkIfAttributeSetAlreadyExist($attribute_set);
+            $attribute_set_id = self::_getAttributeSetId($attribute_set);
 
-            $attribute_id = self::checkIfAttributeAlreadyExist($attribute_set_id, $attribute_alias);
+            if (!$attribute_set_id) {
+                $attribute_set_id = Moraso_Db::put('_eav_attribute_set', 'attribute_set_id', array(
+                            'attribute_set_name' => $attribute_set
+                ));
+            }
+
+            $attribute_id = Moraso_Db::simpleFetch('attribute_id', '_eav_attribute', array(
+                        'attribute_alias' => $attribute_alias,
+                        'attribute_set_id' => $attribute_set_id
+            ));
+
+            if (!$attribute_id) {
+                $attribute_id = Moraso_Db::put('_eav_attribute', 'attribute_id', array(
+                            'attribute_alias' => $attribute_alias,
+                            'attribute_set_id' => $attribute_set_id
+                ));
+            }
 
             if (is_float($value)) {
                 $type = 'float';
@@ -85,174 +47,119 @@ class Moraso_Eav {
                 $type = 'string';
             }
 
-            self::setValue($id, $attribute_id, $value, $type);
+            $entity_attribute_id = Moraso_Db::simpleFetch('entity_attribute_id', '_eav_entity_attribute', array(
+                        'entity_id' => $id,
+                        'attribute_id' => $attribute_id
+            ));
+
+            if (!empty($entity_attribute_id)) {
+                $value_id = Moraso_Db::simpleFetch('value_id', '_eav_value', array(
+                            'entity_attribute_id' => $entity_attribute_id
+                ));
+            } else {
+                $entity_attribute_id = Moraso_Db::put('_eav_entity_attribute', 'entity_attribute_id', array(
+                            'entity_id' => $id,
+                            'attribute_id' => $attribute_id
+                ));
+
+                $value_id = Moraso_Db::put('_eav_value', 'value_id', array(
+                            'entity_attribute_id' => $entity_attribute_id
+                ));
+            }
+
+            if (is_array($value) || is_object($value)) {
+                $value = serialize($value);
+            }
+
+            $data = array(
+                'value_id' => $value_id,
+                'value_' . $type => $value
+            );
+
+            Moraso_Db::delete('_eav_value', array('value_id' => $value_id));
+            Moraso_Db::put('_eav_value', NULL, $data);
         }
     }
 
-    public static function getAttributeIdByAlias($set_id, $alias) {
+    public static function getData($attribute_set, $entity_id = null) {
 
-        return Moraso_Db::fetchOne('' .
-                        'select ' .
-                        '   attribute_id ' .
-                        'from ' .
-                        '   _eav_attribute ' .
-                        'where ' .
-                        '   attribute_alias =:alias ' .
-                        'and ' .
-                        '   attribute_set_id =:set_id', array(
-                    ':alias' => $alias,
-                    ':set_id' => $set_id
-        ));
-    }
+        $attribute_set_id = self::_getAttributeSetId($attribute_set);
 
-    public static function getAttributeSetId($set_name) {
+        $data = array();
 
-        return Moraso_Db::fetchOne('' .
-                        'select ' .
-                        '   attribute_set_id ' .
-                        'from ' .
-                        '   _eav_attribute_set ' .
-                        'where ' .
-                        '   attribute_set_name =:set_name', array(
-                    ':set_name' => $set_name
-        ));
-    }
-
-    public static function setValue($entity_id, $attribute_id, $value, $type = 'string') {
-
-        $entity_attribute_id = Moraso_Db::fetchOne('' .
-                        'select ' .
-                        '   entity_attribute_id ' .
-                        'from ' .
-                        '   _eav_entity_attribute ' .
-                        'where ' .
-                        '   entity_id =:entity_id ' .
-                        'and ' .
-                        '   attribute_id =:attribute_id', array(
-                    ':entity_id' => $entity_id,
-                    ':attribute_id' => $attribute_id
-        ));
-
-
-        if (!empty($entity_attribute_id)) {
-            $value_id = Moraso_Db::fetchOne('' .
+        if (!empty($entity_id)) {
+            $rows = Moraso_Db::fetchAll('' .
                             'select ' .
-                            '   value_id ' .
+                            '   a.attribute_alias, ' .
+                            '   coalesce( ' .
+                            '       v.value_string, ' .
+                            '       v.value_integer, ' .
+                            '       v.value_float ' .
+                            '   ) as value ' .
                             'from ' .
-                            '   _eav_value ' .
+                            '   _eav_entity as e ' .
+                            'left join ' .
+                            '   _eav_entity_attribute as ea on ea.entity_id = e.entity_id ' .
+                            'left join ' .
+                            '   _eav_attribute as a on a.attribute_id = ea.attribute_id ' .
+                            'left join ' .
+                            '   _eav_value as v on v.entity_attribute_id = ea.entity_attribute_id ' .
                             'where ' .
-                            '   entity_attribute_id =:entity_attribute_id ', array(
-                        ':entity_attribute_id' => $entity_attribute_id
+                            '   ea.entity_id =:entity_id ' .
+                            'and ' .
+                            '   a.attribute_set_id =:attribute_set_id', array(
+                        ':attribute_set_id' => $attribute_set_id,
+                        ':entity_id' => $entity_id
                             )
             );
+
+            foreach ($rows as $row) {
+                $data[$row['attribute_alias']] = $row['value'];
+            }
         } else {
-            $entity_attribute_id = Moraso_Db::query('' .
-                            'insert into ' .
-                            '   _eav_entity_attribute ' .
-                            '   (entity_id, attribute_id) ' .
-                            'values ' .
-                            '   (' . $entity_id . ', ' . $attribute_id . ')'
-                    )->getLastInsertId();
+            $rows = Moraso_Db::fetchAll('' .
+                            'select ' .
+                            '   e.entity_id as id, ' .
+                            '   a.attribute_alias, ' .
+                            '   coalesce( ' .
+                            '       v.value_string, ' .
+                            '       v.value_integer, ' .
+                            '       v.value_float ' .
+                            '   ) as value ' .
+                            'from ' .
+                            '   _eav_entity as e ' .
+                            'left join ' .
+                            '   _eav_entity_attribute as ea on ea.entity_id = e.entity_id ' .
+                            'left join ' .
+                            '   _eav_attribute as a on a.attribute_id = ea.attribute_id ' .
+                            'left join ' .
+                            '   _eav_value as v on v.entity_attribute_id = ea.entity_attribute_id ' .
+                            'where ' .
+                            '   a.attribute_set_id =:attribute_set_id', array(
+                        ':attribute_set_id' => $attribute_set_id
+                            )
+            );
 
-            $value_id = Moraso_Db::query('' .
-                            'insert into ' .
-                            '   _eav_value ' .
-                            '   (entity_attribute_id) ' .
-                            'values ' .
-                            '   (' . $entity_attribute_id . ')'
-                    )->getLastInsertId();
-        }
 
-        if (is_array($value) || is_object($value)) {
-            $value = serialize($value);
-        }
+            foreach ($rows as $row) {
+                $data[$row['id']][$row['attribute_alias']] = $row['value'];
+            }
 
-        $data = array(
-            'value_id' => $value_id,
-            'value_' . $type => $value
-        );
+            foreach ($rows as $row) {
+                $data[$row['id']]['id'] = $row['id'];
+            }
 
-        Moraso_Db::query('delete from _eav_value where value_id =:valueid', array(':valueid' => $value_id));
-
-        Moraso_Db::put('_eav_value', NULL, $data);
-    }
-
-    public static function getEntityData($attribute_set, $entity_id) {
-
-        $attribute_set_id = self::getAttributeSetId($attribute_set);
-
-        $rows = Moraso_Db::fetchAll('' .
-                        'select ' .
-                        '   a.attribute_alias, ' .
-                        '   coalesce( ' .
-                        '       v.value_string, ' .
-                        '       v.value_integer, ' .
-                        '       v.value_float ' .
-                        '   ) as value ' .
-                        'from ' .
-                        '   _eav_entity as e ' .
-                        'left join ' .
-                        '   _eav_entity_attribute as ea on ea.entity_id = e.entity_id ' .
-                        'left join ' .
-                        '   _eav_attribute as a on a.attribute_id = ea.attribute_id ' .
-                        'left join ' .
-                        '   _eav_value as v on v.entity_attribute_id = ea.entity_attribute_id ' .
-                        'where ' .
-                        '   ea.entity_id =:entity_id ' .
-                        'and ' .
-                        '   a.attribute_set_id =:attribute_set_id', array(
-                    ':attribute_set_id' => $attribute_set_id,
-                    ':entity_id' => $entity_id
-                        )
-        );
-
-        $data = array();
-        foreach ($rows as $row) {
-            $data[$row['attribute_alias']] = $row['value'];
+            sort($data);
         }
 
         return $data;
     }
 
-    public static function getAllData($set_name) {
+    private static function _getAttributeSetId($set_name) {
 
-        $attribute_set_id = self::getAttributeSetId($set_name);
-
-        $rows = Moraso_Db::fetchAll('' .
-                        'select ' .
-                        '   e.entity_id as id, ' .
-                        '   a.attribute_alias, ' .
-                        '   coalesce( ' .
-                        '       v.value_string, ' .
-                        '       v.value_integer, ' .
-                        '       v.value_float ' .
-                        '   ) as value ' .
-                        'from ' .
-                        '   _eav_entity as e ' .
-                        'left join ' .
-                        '   _eav_entity_attribute as ea on ea.entity_id = e.entity_id ' .
-                        'left join ' .
-                        '   _eav_attribute as a on a.attribute_id = ea.attribute_id ' .
-                        'left join ' .
-                        '   _eav_value as v on v.entity_attribute_id = ea.entity_attribute_id ' .
-                        'where ' .
-                        '   a.attribute_set_id =:attribute_set_id', array(
-                    ':attribute_set_id' => $attribute_set_id
-                        )
-        );
-
-        $data = array();
-        foreach ($rows as $row) {
-            $data[$row['id']][$row['attribute_alias']] = $row['value'];
-        }
-
-        foreach ($rows as $row) {
-            $data[$row['id']]['id'] = $row['id'];
-        }
-
-        sort($data);
-
-        return $data;
+        return Moraso_Db::simpleFetch('attribute_set_id', '_eav_attribute_set', array(
+                    'attribute_set_name' => $set_name
+        ));
     }
 
 }
